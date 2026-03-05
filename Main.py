ff138ab5ae72dce9e2d2267bb6d548f96063840c
AMcybergaming <134148099+AMcybergaming@users.noreply.github.com>
2026-03-05 15:38:07 +0000

    Add project skeleton and basic structure

---
+++ Main.py
@@ -0,0 +1,7 @@
+#Define variables
+
+#Function to enter variables as user input
+
+#Function with if statement that solves values based on operator
+
+#Function to restart from beginning or solve for term
